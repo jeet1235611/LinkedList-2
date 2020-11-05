0de663accda2ed08be6663c43964b10c950931dc
Ankit Bhankharia <71999020+cupcake08@users.noreply.github.com>
2020-11-05 12:16:44 +0530

    Update MergeTwoSortedLL.cpp
    Iterative Method added

--- MergeTwoSortedLL.cpp
+++ MergeTwoSortedLL.cpp
@@ -1,5 +1,5 @@
 //Coding Ninjas Platform Problem Solution
-
+//****************Recursive Method******************//
 Node* mergeTwoSortedLinkedLists(Node *a,Node *b)
 {
   Node *result=NULL;
@@ -20,4 +20,53 @@ Node* mergeTwoSortedLinkedLists(Node *a,Node *b)
     result->next=mergeTwoSortedLinkedLists(a,b->next);
   }
   return result;
+}
+//iteratively Method........*********************************************//
+Node *mergeTwoSortedLinkedLists(Node *head1,Node *head2)
+{
+    if(head1==nullptr)
+    {
+        return head2;
+    }else if(head2==nullptr)
+    {
+        return head1;
+    }
+    Node *finalHead=nullptr;
+    Node *finalTail=nullptr;
+    while(head1!=nullptr && head2 != nullptr)
+    {
+        if(head1->data <= head2->data)
+        {
+            if(finalHead==nullptr)
+            {
+                finalHead=head1;
+                finalTail=head1;
+            }else
+            {
+                finalTail->next=head1;
+                finalTail=head1;
+            }
+            head1=head1->next;
+        }else
+        {
+            if(finalHead==nullptr)
+            {
+                finalHead=head2;
+                finalTail=head2;
+            }else
+            {
+                finalTail->next=head2;
+                finalTail=head2;
+            }
+            head2=head2->next;
+        }
+    }
+    if(head1 == nullptr)
+    {
+        finalTail->next=head2;
+    }else if(head2==nullptr)
+    {
+        finalTail->next=head1;
+    }
+    return finalHead;
 }
