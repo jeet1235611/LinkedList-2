87d752e66e81e89bf092891b62e2c9392c07656f
acoustic_jeet <js.jitendra11@gmail.com>
2022-06-23 00:29:54 +0530

    Create stringPractice.java

---
+++ stringPractice.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.Scanner;
+class HelloWorld {
+    
+        public static void main(String args[]) {
+        char arr[] = {'c', '0', 'd', 'i', 'n', 'g'};
+        String str = "Coding";
+        String str1 = "";
+        System.out.println(str1.length());
+        System.out.println();
+        System.out.println(str);
+        System.out.println(str.charAt(6));
+
+    }
+}
